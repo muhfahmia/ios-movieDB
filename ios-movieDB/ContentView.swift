7eaf7a601b9a2416777e2530279ff368417fc4f0
Fahmi Ardiyanto <mfahmiardiyannto@gmail.com>
2023-11-28 16:37:42 +0700

    Setup Project

---
+++ ios-movieDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-movieDB
+//
+//  Created by Muhammad Fahmi on 28/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-movieDB/ios_movieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_movieDBApp.swift
+//  ios-movieDB
+//
+//  Created by Muhammad Fahmi on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_movieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
